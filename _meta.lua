3a4d17cba05a0ac67c1839cebd957b3c66819cd9
Chandu <14921501+chandujr@users.noreply.github.com>
2026-09-11 12:41:11 +0530

    Update readme and add gitattributes file

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,6 @@
 local _ = require("gettext")
 return {
     fullname = _("WikiReader"),
-    description = _([[Read Wikipedia articles as formatted books, with an improved experience over KOReader's built-in Wikipedia lookup.]]),
+    description = _([[Read Wikipedia articles as formatted books, with a cleaner reading experience than KOReader's built-in Wikipedia lookup.]]),
     version = "1.0.0",
 }
